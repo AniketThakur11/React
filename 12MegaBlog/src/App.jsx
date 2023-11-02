f69233e1aeeece9b6748347c317eb87fd85f923a
Aniket <aniket.1@kochartech.com>
2023-11-02 15:31:31 +0530

    Successfully Addedd

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+// useDispatch is the combination to use react with redux
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>React With AppWrite </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min -h-screen flex flex-wrap content-between bg-gray-300'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* {outlet} */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
